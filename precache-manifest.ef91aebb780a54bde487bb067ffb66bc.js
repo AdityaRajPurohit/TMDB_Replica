fcca203815e9d581a84d372a2043eba12124f345
AdityaRaj Purohit <purohit.adiraj30@gmail.com>
2019-07-05 18:06:29 +0530

    Updates

---
+++ precache-manifest.ef91aebb780a54bde487bb067ffb66bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "620411b021b8112e80a296fedf80ee6f",
+    "url": "/AdityaRajPurohit.github.io/TMDB_Replica-/index.html"
+  },
+  {
+    "revision": "fe88621e59c93d5e027d",
+    "url": "/AdityaRajPurohit.github.io/TMDB_Replica-/static/css/main.22622f7a.chunk.css"
+  },
+  {
+    "revision": "8b66700dab28c3b3a40f",
+    "url": "/AdityaRajPurohit.github.io/TMDB_Replica-/static/js/2.a9611dfb.chunk.js"
+  },
+  {
+    "revision": "fe88621e59c93d5e027d",
+    "url": "/AdityaRajPurohit.github.io/TMDB_Replica-/static/js/main.619b027f.chunk.js"
+  },
+  {
+    "revision": "a9c5f94d51e7d19c73ee",
+    "url": "/AdityaRajPurohit.github.io/TMDB_Replica-/static/js/runtime~main.4bf22bc9.js"
+  }
+]);
